a45ea56ace4fc85b598c6b6ca5e9eeef8bcd7c67
student <student@M3034>
2024-09-06 14:28:38 +0900

    20240906

--- IBATISProject/src/test/java/com/multicampus/biz/board/BoardDAOClient.java
+++ IBATISProject/src/test/java/com/multicampus/biz/board/BoardDAOClient.java
@@ -1,17 +1,22 @@
 package com.multicampus.biz.board;
 
-import java.sql.SQLException;
 import java.util.List;
 
 public class BoardDAOClient {
 
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) throws Exception {
 		BoardDAO boardDAO = new BoardDAO();
 
-//		List<BoardVO> boardList = boardDAO.getBoardList();
-//		for (BoardVO board : boardList) {
-//			System.out.println("---> " + board.toString());
-//		}
+		BoardVO vo = new BoardVO();
+		vo.setTitle("iBATIS 제목");
+		vo.setWriter("xxx");
+		vo.setContent("테스트중입");
+		boardDAO.insertBoard(vo);
+		
+		List<BoardVO> boardList = boardDAO.getBoardList(vo);
+		for (BoardVO board : boardList) {
+			System.out.println("---> " + board.toString());
+		}
 	}
 
 }
